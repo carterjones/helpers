7555c9d61fe9810ee75e8a30dc5aadb39e94bb57
Carter Jones <carter@carterjones.info>
2018-02-04 16:56:53 -0800

    remove debug and error related functions

--- trace/trace.go
+++ trace/trace.go
@@ -14,11 +14,6 @@ func traceEnabled() bool {
 	return strings.ToLower(v) == "true"
 }
 
-func debugEnabled() bool {
-	v := os.Getenv("DEBUG")
-	return strings.ToLower(v) == "true"
-}
-
 // Line returns the function name and line number wherever Line() is
 // called. This is most useful when creating a trail of call executions across
 // multiple goroutines.
@@ -32,43 +27,4 @@ func Line() {
 	frames := runtime.CallersFrames(pc[:n])
 	frame, _ := frames.Next()
 	log.Printf("%s:%d\n", frame.Function, frame.Line)
-}
-
-// Error returns the function name, line number wherever Error() is called, and
-// the supplied error message. This is most useful when creating a trail of
-// errors across multiple goroutines.
-func Error(err error) {
-	if !traceEnabled() {
-		return
-	}
-
-	pc := make([]uintptr, 15)
-	n := runtime.Callers(2, pc)
-	frames := runtime.CallersFrames(pc[:n])
-	frame, _ := frames.Next()
-	log.Printf("%s:%d: %s\n", frame.Function, frame.Line, err.Error())
-}
-
-// DebugLine is the same as Line(), except that it only prints out information
-// when the DEBUG environment variable is set to "true".
-func DebugLine() {
-	if !debugEnabled() {
-		return
-	}
-
-	pc := make([]uintptr, 15)
-	n := runtime.Callers(2, pc)
-	frames := runtime.CallersFrames(pc[:n])
-	frame, _ := frames.Next()
-	log.Printf("%s:%d\n", frame.Function, frame.Line)
-}
-
-// DebugMessage prints the supplied message only when the DEBUG environment
-// variable is set to "true".
-func DebugMessage(msg string, v ...interface{}) {
-	if !debugEnabled() {
-		return
-	}
-
-	log.Printf(msg, v...)
 }
